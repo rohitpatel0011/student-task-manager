b9a87b3cffe52478f8877dad4d53e7a64e87637e
RohitPatel00111 <rajgolu636@gmail.com>
2026-01-03 23:32:44 +0530

    domain change

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
-}));j
+}));
 
 app.use(express.json());
 
